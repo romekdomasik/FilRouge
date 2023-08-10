bd1dd8b5509b7466406ed893cfd924c31c9f5b06
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-08-10 16:21:23 +0200

    passage de l'authentication au home
    passage de l'authentication au home grâce au bouton

--- app/src/main/java/com/example/filrouge/AuthenticationActivity.kt
+++ app/src/main/java/com/example/filrouge/AuthenticationActivity.kt
@@ -1,7 +1,9 @@
 package com.example.filrouge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.filrouge.databinding.ActivityAuthenticationBinding
 
 class AuthenticationActivity : AppCompatActivity() {
@@ -10,5 +12,32 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthenticationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnConnect.setOnClickListener {
+            var email = binding.champEmail.text
+            var pwd = binding.champPassword.text
+            if (!email.isNullOrBlank() && !pwd.isNullOrBlank()){
+                displayHomeActivity()
+            } else{
+                alert("Vérifiez mot de passe et/ou email", "Erreur")
+            }
+
+        }
     }
+
+    fun displayHomeActivity(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun alert(message: String, title: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage(message)
+            .setTitle(title)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+
+
 }
