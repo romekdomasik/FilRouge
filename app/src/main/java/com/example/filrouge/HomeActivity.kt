d610abcea26f4a130b8d59ba9a71ad746bf9711a
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-08-10 16:10:39 +0200

    home activity
    home activity created

---
+++ app/src/main/java/com/example/filrouge/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.filrouge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.filrouge.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
