143c6ab6503a6025445d6422d574de9ff4ff0dbd
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-08-10 22:38:40 +0200

    BOTTOM BAR MENU !
    Enfin fait en sorte que la bottom bar change réellement de page

--- app/src/main/java/com/example/filrouge/HomeActivity.kt
+++ app/src/main/java/com/example/filrouge/HomeActivity.kt
@@ -2,6 +2,10 @@ package com.example.filrouge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.filrouge.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,4 +15,21 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        val bottomNavigationView = binding.bottomNavigationView
+        val navControler = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNavigationView, navControler)
+    }
+
+    /*override fun onPostCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onPostCreate(savedInstanceState, persistentState)
+        setupBottomNavigation()
+
+    }
+    private fun setupBottomNavigation() {
+        val navController = Navigation.findNavController(this, binding.navHostFragment.id)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+    }*/
 }
